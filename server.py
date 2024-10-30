14f2ee3bc4d9816e226afe2df1f3e88e084dd70b
jalknKlith <alejalkndro@gmail.com>
2024-10-30 14:59:58 -0500

    sqlConnection

--- server.py
+++ server.py
@@ -1,8 +1,33 @@
 import flask as fk
 import weather as wr
 import waitress as wss
+import mysql.connector as sql
+import datetime as dt
 
 app = fk.Flask(__name__)
+
+def getDBconnection():
+    conn = sql.connect(
+        host='localhost',
+        user='root',
+        password='mefort@1ece',
+        database='climapp'
+    )
+    return conn
+
+def guardarDatos(ciudad,temperatura,descripcion):
+    conn = getDBconnection()
+    cur = conn.cursor()
+    now = dt.datetime.now()
+    try:
+        cur.execute('INSERT INTO clima (ciudad, temperatura, descripcion, fecHora) VALUES (%s, %s, %s, %s)',(ciudad, temperatura, descripcion, now))
+        conn.commit()
+    except sql.Error as err:
+        print(f"Error al guardar los datos: {err}")
+        conn.rollback()
+    finally:
+        cur.close()
+        conn.close()
 
 @app.route('/')
 @app.route('/index')
@@ -23,6 +48,8 @@ def get_weather():
 
     if weather_data['cod'] != 200:
         return fk.render_template('error.html', ciudad=city), 404
+    
+    guardarDatos(weather_data["name"], weather_data['main']['temp'],weather_data["weather"][0]["description"])
 
     return fk.render_template(
         "weather.html",
@@ -31,6 +58,25 @@ def get_weather():
         temp=f"{weather_data['main']['temp']:.1f}",
         feels_like=f"{weather_data['main']['feels_like']:.1f}"
     )
-
+    
+def creaTable():
+    conn = getDBconnection()
+    cur = conn.cursor()
+    try:
+        cur.execute('''CREATE TABLE IF NOT EXISTS clima (
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        ciudad VARCHAR(255) NOT NULL,
+                        temperatura FLOAT,
+                        descripcion VARCHAR(255),
+                        fecHora DATETIME
+                    )''')
+        conn.commit()
+    except sql.Error as err:
+        print(f"Error al crear la tabla: {err}")
+    finally:
+        cur.close()
+        conn.close()
+        
 if __name__ == "__main__":
+    creaTable()
     wss.serve(app, host="0.0.0.0", port=8000)
